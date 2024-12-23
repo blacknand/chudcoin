2bebf4c67e4670f06b044f428ca888492752dbc3
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-12-23 01:04:01 +0000

    add utc data

--- src/block.cpp
+++ src/block.cpp
@@ -1,18 +1,13 @@
 #include "block.h"
 #include "utils.h"
+#include "utc_data.h"
 #include <ctime>
 #include <chrono>
 #include <iostream>
+#include <iomanip>
 
 Block::Block(int idx, const std::string& prev_hash)
     : index(idx), previous_hash(prev_hash), nonce(0) {
-    // TODO: Get current time
-    auto start = std::chrono::system_clock::now();
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end-start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
- 
-    std::cout << "finished computation at " << std::ctime(&end_time)
-              << "elapsed time: " << elapsed_seconds.count() << "s"
-              << std::endl;
+    timestamp = get_utc_time();
+    std::cout << timestamp << std::endl;
 }
