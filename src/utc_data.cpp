e18a4431ec312d6ab7541f66d704b90bbd045782
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-01-02 22:02:33 +0000

    add cross platform compiler preprocessor directives

--- src/utc_data.cpp
+++ src/utc_data.cpp
@@ -11,7 +11,15 @@ std::string get_utc_time() {
     const auto now = std::chrono::system_clock::now();
     const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
     tm utc_time_struct;
-    tm* utc_time = gmtime_r(&t_c, &utc_time_struct);
+
+    tm* utc_time = nullptr;
+    #if defined(__APPLE__) || defined(__linux__)  // POSIX platforms (macOS, Linux)
+        utc_time = gmtime_r(&t_c, &utc_time_struct);
+    #elif defined(_WIN32)  // Windows
+        gmtime_s(&utc_time_struct, &t_c);  // gmtime_s does not return a pointer
+        utc_time = &utc_time_struct;
+    #endif
+    
     std::ostringstream oss;
     oss << "UTC time: " << 1900 + utc_time->tm_year << "-"
             << 1 + utc_time->tm_mon << "-"
